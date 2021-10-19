b332dfc0fa5fad8938185a513105ad21dea2ab00
Katie Devops <angel_9586@hotmail.com>
2021-10-19 11:50:42 -0500

    first commit

--- objectOrientationInteractive.js
+++ objectOrientationInteractive.js
@@ -1,6 +1,83 @@
-let person ={
+let person = {
     firstName: 'Chandler',
     lastName:  'Bong' ,
     age :       34
 }
- console.log(person.firstName )
+// console.log(person.firstName)
+
+let meal = {
+    appetizer: 'chips & salsa',
+    entree:    'all pastor burrito',
+    dessert:    'churros',
+    drink:     'Dr. Pepper',
+}
+
+const {dessert} = meal
+
+//console.log(dessert)
+
+const {appetizer, entree} = meal 
+
+//console.log(appetizer)
+//console.log( entree)
+
+const {drink:bestSodaEver} = meal
+
+//console.log(bestSodaEver)
+
+//const myAppetizer = meal.appetizer
+//const myEntree = meal.entree
+//const myDessert = meal.dessert
+//const myDrink = meal.drink
+
+const{appetizer: myAppetizer, entree: myEntree, dessert: myDessert, drink: myDrink}
+=meal
+
+//console.log (bestSodaEver)
+//console.log (myDrink)
+
+//for (key in person){
+ //   console.log(key)
+//}
+
+//for (key in person){
+ //   console.log(person[key])
+//}
+
+person.job ="Statistical Analasys and Data Reconfiguration"
+
+person["pets"] = ['Chicken', "Duck"]
+
+//for (key in person){
+ //   console.log(key)
+//}
+
+let teams = {
+    teamOne : ['ryan', 'alex'],
+    teamTwo : ['henry', 'charlie'],
+    teamThree: ['porter', 'blake'],
+    teamFour: ['monique', 'mariko'],
+    teamFive: ['shania' , 'deepta']
+}
+
+delete teams.teamsTwo
+
+//for (let team in teams) {
+  ///  console.log(team)
+//}
+
+class Dog {
+    constructor(name, breed, age){
+        this.name = name
+        this.breed = breed
+        this.age = age 
+    }
+}
+
+let lassie = new Dog ('Lassie', 'Collie', '18')
+
+//console.log(lassie.name)
+
+let beethoven = new Dog ( 'Beethoven', 'St. Bernard', '12')
+
+console.log(beethoven.breed)
